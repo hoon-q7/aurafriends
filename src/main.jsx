8ed17733405e93d5104134e5ecf48ae6e5149ef3
hoon-q7 <dlqudgns1997@gmail.com>
2026-03-30 20:20:09 +0900

    Fix formatting in main.jsx
    Formatted import statements and adjusted indentation for rendering.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from ‘react’
-import ReactDOM from ‘react-dom/client’
-import AuraFriends from ‘./App.jsx’
-import ‘./index.css’
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AuraFriends from './App.jsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById(‘root’)).render(
-<React.StrictMode>
-<AuraFriends />
-</React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuraFriends />
+  </React.StrictMode>
+);
